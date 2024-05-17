7ee88c0646e568bff3769cef2230d7f7ed91f749
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2024-05-18 01:07:43 +0530

    complete push notification setup for chat

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,61 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
 exports.sendNotification = functions.firestore
   .document("chats/{chatId}")
   .onUpdate(async (change, context) => {
-    const data = change.after.data();
-    const previousData = change.before.data();
-    const chatId = context.params.chatId;
-    const lastMessage = data.messages[data.messages.length - 1];
-    const lastMessageContent = lastMessage.content;
-    const senderId = lastMessage.senderId;
-    const participants = data.participants;
-    const receiverId = participants.filter((id) => id !== senderId)[0];
+    try {
+      const data = change.after.data();
+      const previousData = change.before.data();
+      const chatId = context.params.chatId;
+      const lastMessage = data.messages[data.messages.length - 1];
+      const lastMessageContent = lastMessage.content;
+      const senderId = lastMessage.senderId;
+      const participants = data.participants;
+      const receiverId = participants.filter((id) => id !== senderId)[0];
+      const querySnapshot = await admin
+        .firestore()
+        .collection("users")
+        .where("userId", "in", [receiverId, senderId])
+        .get();
+      if (querySnapshot.empty) {
+        return;
+      }
 
-    const querySnapshot = await admin
-      .firestore()
-      .collection("users")
-      .where("id", "in", [receiverId, senderId])
-      .get();
+      const receiverUser = querySnapshot.docs.find(
+        (doc) => doc.data().userId === receiverId
+      );
 
-    if (querySnapshot.empty) {
-      return;
+      const receiverFcmToken = receiverUser.data().token;
+      const senderUser = querySnapshot.docs.find(
+        (doc) => doc.data().userId === senderId
+      );
+      const senderName = senderUser.data().name;
+      const senderPhotoURL = senderUser.data().photoUrl;
+      console.log(
+        "Sending notification to",
+        receiverId,
+        "with token",
+        receiverFcmToken,
+        "from",
+        senderId,
+        "with name",
+        senderName,
+        "and photo",
+        senderPhotoURL
+      );
+      const message = {
+        token: receiverFcmToken,
+        notification: {
+          title: senderName,
+          body: lastMessageContent,
+        },
+        data: { photoUrl: senderPhotoURL },
+      };
+      const notificationResponse = await admin.messaging().send(message);
+      console.log("notificationResponse", notificationResponse);
+    } catch (e) {
+      console.log(e);
     }
-
-    const receiverUser = querySnapshot.docs.filter((doc) => {
-      return doc.data().id === receiverId;
-    });
-    const receiverFcmToken = receiverUser.token;
-
-    const senderUser = querySnapshot.docs.filter((doc) => {
-      return doc.data().id === senderId;
-    });
-    const senderName = senderUser.data().name;
-    const senderPhotoURL = senderUser.data().photoUrl;
-
-    const message = {
-      token: receiverFcmToken,
-      notification: {
-        title: senderName,
-        body: lastMessageContent,
-      },
-      data: { photoUrl: senderPhotoURL },
-    };
-    await admin.messaging().send(message);
   });
